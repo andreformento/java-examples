8d63e062130764c80c6a9589c719c2bc277a91fa
André Formento <andreformento.sc@gmail.com>
2025-10-27 23:32:45 -0300

    fix tests

--- web/src/test/java/com/formento/web/WebApplicationTests.java
+++ web/src/test/java/com/formento/web/WebApplicationTests.java
@@ -1,9 +1,12 @@
 package com.formento.web;
 
+import com.formento.web.config.TestContainersConfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.context.ImportTestcontainers;
 
 @SpringBootTest
+@ImportTestcontainers(TestContainersConfig.class)
 class WebApplicationTests {
 
 	@Test
